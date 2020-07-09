abe7a1807ac448fa39ea4da2a59efd9ad13d55f9
kyleIdeum <40867339+kyleIdeum@users.noreply.github.com>
2020-07-09 09:37:39 -0600

    Move projects and dependencies from internal repo

---
+++ src/AddOn/Assets/_App/Scripts/App.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Ideum;
+using Ideum.Data;
+using System.IO;
+using UnityEngine;
+using Newtonsoft.Json.Linq;
+
+public class App : MonoBehaviour {
+
+  private bool _connected;
+  private float _queryInterval = 0.25f;
+  private float _timer;
+
+  public UIController UIController;
+  public RectTransform Scalar;
+
+  void Awake() {
+    try {
+      var path = Path.Combine(Application.streamingAssetsPath, "settings.json");
+      if (File.Exists(path)) {
+        var json = File.ReadAllText(path);
+        var obj = JObject.Parse(json);
+        if (int.TryParse(obj["ScreenNumber"].ToString(), out var screen)) {
+          Log.Info("Screen set to " + screen);
+        }
+      }
+    }
+    catch (Exception e) {
+      Log.Error(e);
+    }
+
+#if !UNITY_EDITOR
+        if(Display.displays.Length > 1)
+        {
+            PlayerPrefs.SetInt("UnitySelectMonitor", ScreenNumber);
+
+            var display = Display.displays[ScreenNumber];
+            int w = display.systemWidth;
+            int h = display.systemHeight;
+            Screen.SetResolution(w, h, true);
+        }
+        else
+        {
+            Application.Quit();
+        } 
+#endif
+    TouchlessDesign.Initialize(AppSettings.Get().DataDirectory.GetPath());
+    TouchlessDesign.Connected += OnConnected;
+    TouchlessDesign.Disconnected += OnDisconnected;
+  }
+
+  void OnApplicationQuit() {
+    TouchlessDesign.DeInitialize();
+  }
+
+  private void OnDisconnected() {
+    Log.Info("Disconnected. Suspending queries");
+    _connected = false;
+  }
+
+  private void OnConnected() {
+    Log.Info("Connected. Starting to query...");
+    _connected = true;
+    TouchlessDesign.QueryAddOn(HandleAddOnQuery);
+  }
+
+  private void HandleAddOnQuery(bool hasScreen, bool hasLEDs, int width_px, int height_px, int width_mm, int height_mm) {
+    Log.Info($"{hasScreen}, {hasLEDs}, {width_px}, {height_px}, {width_mm}, {height_mm}");
+    if (!hasScreen) return;
+    float scaledX = Scalar.localScale.x * (width_mm / height_mm) / (width_px / height_px);
+    Scalar.localScale = new Vector2(scaledX, Scalar.localScale.y);
+  }
+
+  private void Update() {
+    if (_connected) {
+      _timer += Time.deltaTime;
+      if (_timer > _queryInterval) {
+        TouchlessDesign.QueryClickAndHoverState(HandleQueryResponse);
+        TouchlessDesign.QueryNoTouchState(HandleNoTouchState);
+        _timer = 0f;
+      }
+    }
+  }
+
+  private void HandleNoTouchState(bool noTouch) {
+    UIController.NoTouchWarning(noTouch);
+  }
+
+  private void HandleQueryResponse(bool clickState, HoverStates hoverState) {
+    UIController.DoStateChange(hoverState, clickState);
+  }
+}
---
+++ src/Cursor/Assets/_App/Scripts/App.cs
@@ -0,0 +1,56 @@
+﻿using Ideum.Data;
+using UnityEngine;
+
+namespace Ideum {
+  public class App : MonoBehaviour {
+
+    public Cursor Cursor;
+
+    private bool _connected;
+    private float _queryInterval = 0.25f;
+    private float _timer;
+
+    void Start() {
+      TouchlessDesign.Initialize(AppSettings.Get().DataDirectory.GetPath());
+      TouchlessDesign.Connected += OnConnected;
+      TouchlessDesign.Disconnected += OnDisconnected;
+    }
+
+    void OnApplicationQuit() {
+      TouchlessDesign.DeInitialize();
+    }
+
+    private void OnDisconnected() {
+      Log.Info("Disconnected. Suspending queries");
+      Cursor.DoStateChange(HoverStates.None, false);
+      _connected = false;
+    }
+
+    private void OnConnected() {
+      Log.Info("Connected. Starting to query...");
+      _connected = true;
+    }
+
+    private void Update() {
+      if (_connected) {
+        _timer += Time.deltaTime;
+        if(_timer > _queryInterval) {
+          TouchlessDesign.QueryClickAndHoverState(HandleQueryResponse);
+          TouchlessDesign.QueryNoTouchState(HandleNoTouch);
+          _timer = 0f;
+        }
+      }
+    }
+
+    private void HandleNoTouch(bool noTouch) {
+      if (noTouch) {
+        Cursor.ShowNoTouch();
+      } 
+    }
+
+    private void HandleQueryResponse(bool clickState, HoverStates hoverState) {
+      Cursor.DoStateChange(hoverState, clickState);
+    }
+
+  }
+}
